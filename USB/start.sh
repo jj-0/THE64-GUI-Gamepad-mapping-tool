af6eaefbd10c9daf9f81cdb03af158d2f3738ab4
jj-0 <37620359+jj-0@users.noreply.github.com>
2026-02-12 18:13:26 +0100

    Add fake firmware update files

---
+++ USB/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Kill the64
+until [ `ps | grep -e the64 | wc -l` -eq 1 ]
+do
+        killall the64
+done
+
+mount -o remount,rw /mnt
+cd /mnt
+
+./switchEGLbuffer 1280 720
+./gamepad_map
+
+the64 &
